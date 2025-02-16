4baa47501aee4881a1ae1c19185dbdc9ef6eca71
nirjonroy <roynirjon18@gmail.com>
2025-02-16 15:03:28 +0600

    footer added

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -1,138 +1,106 @@
- <!--Footer-->
- <footer
- class="relative bg-gradient-to-r from-orange-500 to-red-500 pt-8 pb-6"
->
- <div class="max-w-7xl mx-auto px-4">
-   <div class="flex flex-wrap text-left lg:text-left">
-     <div class="w-full lg:w-6/12 px-4">
-       <h4 class="text-3base text-white font-semibold text-blueGray-700">
-         Let's stay connected!
-       </h4>
-       <h5 class="text-lg mt-0 mb-2 text-white">
-         Find us on any of these platforms. We usually respond within 1-2
-         business days.
-       </h5>
-       <div class="mt-6 lg:mb-0 mb-6">
-         <button
-           class="bg-white text-lightBlue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
-           type="button"
-         >
-           <i class="fab fa-twitter"></i>
-         </button>
-         <button
-           class="bg-white text-lightBlue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
-           type="button"
-         >
-           <i class="fab fa-facebook-square"></i>
-         </button>
-         <button
-           class="bg-white text-pink-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
-           type="button"
-         >
-           <i class="fab fa-dribbble"></i>
-         </button>
-         <button
-           class="bg-white text-blueGray-800 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
-           type="button"
-         >
-           <i class="fab fa-github"></i>
-         </button>
-       </div>
-     </div>
-     <div class="w-full lg:w-6/12 px-4">
-       <div class="flex flex-wrap items-top mb-6">
-         <div class="w-full lg:w-4/12 px-4 ml-auto">
-           <span
-             class="block uppercase text-white text-lg font-semibold mb-4"
-             >Useful Links</span
-           >
-           <ul class="list-unstyled">
-             <li>
-               <a
-                 class="text-white hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                 href="#"
-                 >About Us</a
-               >
-             </li>
-             <li>
-               <a
-                 class="text-white hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                 href="#"
-                 >Blog</a
-               >
-             </li>
-             <li>
-               <a
-                 class="text-white hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                 href="appetizer.html"
-                 >Appetizer Foods</a
-               >
-             </li>
-             <li>
-               <a
-                 class="text-white hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                 href="#"
-                 >Best Foods</a
-               >
-             </li>
-           </ul>
-         </div>
-         <div class="w-full lg:w-4/12 px-4">
-           <span
-             class="block uppercase text-white text-lg font-semibold mb-4"
-             >Other Resources</span
-           >
-           <ul class="list-unstyled">
-             <li>
-               <a
-                 class="text-white hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                 href="#"
-                 >Terms & Conditions</a
-               >
-             </li>
-             <li>
-               <a
-                 class="text-white hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                 href="#"
-                 >Privacy Policy</a
-               >
-             </li>
-             <li>
-               <a
-                 class="text-white hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                 href="#"
-                 >Contact Us</a
-               >
-             </li>
-           </ul>
-         </div>
-       </div>
-     </div>
-   </div>
-   <hr class="my-6 border-blueGray-300" />
-   <div
-     class="flex flex-wrap items-center md:justify-between justify-center"
-   >
-     <div class="w-full md:w-4/12 px-4 mx-auto text-center">
-       <div class="text-sm text-white font-semibold py-1">
-         Developed <span id="get-current-year">By</span>
-         <a
-           href="https://blacktechcorp.com/"
-           class="text-white hover:text-gray-800"
-           target="_blank"
-           >BlackTech</a
-         >
-         <a
-           href="https://blacktechcorp.com/"
-           class="text-white hover:text-blueGray-800"
-           >Consultancy</a
-         >.
-       </div>
-     </div>
-   </div>
- </div>
-</footer>
-<!--End Footer-->
+<div class="ms_player_wrapper">
+  <div class="ms_player_close">
+      <i class="fa fa-angle-down" aria-hidden="true"></i>
+  </div>
+  <div class="player_mid">
+      <div class="audio-player">
+          <div id="jquery_jplayer_1" class="jp-jplayer"></div>
+          <div id="jp_container_1" class="jp-audio" role="application" aria-label="media player">
+              <div class="player_left">
+                  <div class="ms_play_song">
+                      <div class="play_song_name">
+                          <a href="javascript:void(0);" id="playlist-text">
+                              <div class="jp-now-playing flex-item">
+                                  <div class="jp-track-name"></div>
+                                  <div class="jp-artist-name"></div>
+                              </div>
+                          </a>
+                      </div>
+                  </div>
+                  <div class="play_song_options">
+                      <ul>
+                          <li><a href="#"><span class="song_optn_icon"><i class="ms_icon icon_download"></i></span>download now</a></li>
+                          <li><a href="#"><span class="song_optn_icon"><i class="ms_icon icon_fav"></i></span>Add To Favourites</a></li>
+                          <li><a href="#"><span class="song_optn_icon"><i class="ms_icon icon_playlist"></i></span>Add To Playlist</a></li>
+                          <li><a href="#"><span class="song_optn_icon"><i class="ms_icon icon_share"></i></span>Share</a></li>
+                      </ul>
+                  </div>
+                  <span class="play-left-arrow"><i class="fa fa-angle-right" aria-hidden="true"></i></span>
+              </div>
+              <!----Right Queue---->
+              <div class="jp_queue_wrapper">
+                  <span class="que_text ms_btn" id="myPlaylistQueue"><i class="fa fa-angle-up" aria-hidden="true"></i> queue</span>
+                  <div id="playlist-wrap" class="jp-playlist">
+                      <div class="jp_queue_cls"><i class="fa fa-angle-down" aria-hidden="true"></i></div>
+                      <h2>queue</h2>
+                      <div class="jp_queue_list_inner">
+                          <ul>
+                              <li>&nbsp;</li>
+                          </ul>
+                      </div>
+                      <div class="jp_queue_btn">
+                          <a href="clear_modal" class="ms_save ms_btn" data-toggle="modal" data-target="#save_modal">Save Playlist</a>
+                          <a href="javascript:;" class="ms_clear" data-toggle="modal" data-target="#clear_modal">clear</a>
+                      </div>
+                  </div>
+              </div>
+              <div class="jp-type-playlist">
+                  <div class="jp-gui jp-interface flex-wrap">
+                      <div class="jp-controls flex-item">
+                          <button class="jp-previous" tabindex="0">
+<i class="ms_play_control"></i>
+</button>
+                          <button class="jp-play" tabindex="0">
+<i class="ms_play_control"></i>
+</button>
+                          <button class="jp-next" tabindex="0">
+<i class="ms_play_control"></i>
+</button>
+                      </div>
+                      <div class="jp-progress-container flex-item">
+                          <div class="jp-time-holder">
+                              <span class="jp-current-time" role="timer" aria-label="time">&nbsp;</span>
+                              <span class="jp-duration" role="timer" aria-label="duration">&nbsp;</span>
+                          </div>
+                          <div class="jp-progress">
+                              <div class="jp-seek-bar">
+                                  <div class="jp-play-bar">
+                                      <div class="bullet">
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                      <div class="jp-volume-controls flex-item">
+                          <div class="widget knob-container">
+                              <div class="knob-wrapper-outer">
+                                  <div class="knob-wrapper">
+                                      <div class="knob-mask">
+                                          <div class="knob d3"><span></span></div>
+                                          <div class="handle"></div>
+                                          <div class="round">
+                                              <img src="assets/images/svg/volume.svg" alt="volume">
+                                          </div>
+                                      </div>
+                                  </div>
+                                  <!-- <input></input> -->
+                              </div>
+                          </div>
+                      </div>
+                      <div class="jp-toggles flex-item">
+                          <button class="jp-shuffle" tabindex="0" title="Shuffle">
+        <i class="ms_play_control"></i></button>
+                          <button class="jp-repeat" tabindex="0" title="Repeat"><i class="ms_play_control"></i></button>
+                      </div>
+
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+  <!--main div-->
+</div>
+
 </div>
 
 <!-- bootstrap 5 cdn js  -->
